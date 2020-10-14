d056478f1dd3658e9f4a048ebb95b969bf7f9e88
Serhii777 <muzykasv72@gmail.com>
2020-10-14 21:07:26 +0300

    Added strucure

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import Searchbar from "./Searchbar";
+
+
+export default class App extends Component {
+  state = {};
+
+  render() {
+    return <></>;
+  }
+}
