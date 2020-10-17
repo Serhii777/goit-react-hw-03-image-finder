60021b4f8880746656d0e5c2be4a915a9f3faa17
Serhii777 <muzykasv72@gmail.com>
2020-10-17 11:21:42 +0300

    Updates

---
+++ precache-manifest.99973ec9fe932b0191c18ad23738b084.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ba97f11780d2688e7a3ae9b32e5564f",
+    "url": "/goit-react-hw-03-image-finder/index.html"
+  },
+  {
+    "revision": "9f006d6cfd25e976584e",
+    "url": "/goit-react-hw-03-image-finder/static/css/main.d3a142cb.chunk.css"
+  },
+  {
+    "revision": "2b53614d2d9191390ef6",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.033a92b0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.033a92b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f006d6cfd25e976584e",
+    "url": "/goit-react-hw-03-image-finder/static/js/main.bb440dd3.chunk.js"
+  },
+  {
+    "revision": "00ed64b4697a879a9e95",
+    "url": "/goit-react-hw-03-image-finder/static/js/runtime-main.4e03020b.js"
+  }
+]);
